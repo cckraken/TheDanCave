56c44db863f837ca5e3929245d74241be4d713b6
Dan <d4nn-b@hotmail.com>
2017-12-28 18:39:45 +0000

    Major changes
    - Added additional html content on all pages.
    - Included small nav bar in all footer elements to contain social media links.
    - Numerous styling changes
    - JavaScript added to show popup image when hovering over some text on index page

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,53 @@
-// When the user clicks on <div>, open the popup
-function funcPersonal() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
+/*File for JavaScript functions*/
+
+/*Below 5 functions handle the mouseover events for the title pages. When the text is hovered over, a small image containing what is on the page is shown. Hidden when not hovered over*/
+
+$(document).ready(function() {
+    $(".personal a").mouseover(function () {
+        $(".personal a img").css("display", "none"); // hide all product images
+        $(this).find("img").css("display", "inline-block"); // show current hover image
+    })
+    $(".personal a").mouseout(function () {
+        $(".personal a img").css("display", "none"); // hide all product images
+    })
+});
+
+$(document).ready(function() {
+    $(".university a").mouseover(function () {
+        $(".university a img").css("display", "none"); // hide all product images
+        $(this).find("img").css("display", "inline-block"); // show current hover image
+    })
+    $(".university a").mouseout(function () {
+        $(".university a img").css("display", "none"); // hide all product images
+    })
+});
+
+$(document).ready(function() {
+    $(".dailypost a").mouseover(function () {
+        $(".dailypost a img").css("display", "none"); // hide all product images
+        $(this).find("img").css("display", "inline-block"); // show current hover image
+    })
+    $(".dailypost a").mouseout(function () {
+        $(".dailypost a img").css("display", "none"); // hide all product images
+    })
+});
+
+$(document).ready(function() {
+    $(".learningtidbits a").mouseover(function () {
+        $(".learningtidbits a img").css("display", "none"); // hide all product images
+        $(this).find("img").css("display", "inline-block"); // show current hover image
+    })
+    $(".learningtidbits a").mouseout(function () {
+        $(".learningtidbits a img").css("display", "none"); // hide all product images
+    })
+});
+
+$(document).ready(function() {
+    $(".music a").mouseover(function () {
+        $(".music a img").css("display", "none"); // hide all product images
+        $(this).find("img").css("display", "inline-block"); // show current hover image
+    })
+    $(".music a").mouseout(function () {
+        $(".music a img").css("display", "none"); // hide all product images
+    })
+});
